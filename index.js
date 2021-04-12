0051fe04506594aa351ea38790490da2129f86a8
Trista Runions <t.runions19@gmail.com>
2021-04-11 21:41:41 -0700

    finished task 1

--- index.js
+++ index.js
@@ -7,13 +7,43 @@ Practice accessing data by console.log-ing the following pieces of data note, yo
 
 //(a) Home Team name for 2014 world cup final
 
+console.log(fifaData
+    .filter(i => i["Year"] === 2014 && i["Stage"] === "Final")
+    .map(i => i["Home Team Name"])
+);
+
 //(b) Away Team name for 2014 world cup final
+
+console.log(fifaData
+    .filter(i => i["Year"] === 2014 && i["Stage"] === "Final")
+    .map(i => i["Away Team Name"])
+);
 
 //(c) Home Team goals for 2014 world cup final
 
+console.log(fifaData
+    .filter(i => i["Year"] === 2014 && i["Stage"] === "Final")
+    .map(i => i["Home Team Goals"])
+);
+
 //(d) Away Team goals for 2014 world cup final
 
+console.log(fifaData
+    .filter(i => i["Year"] === 2014 && i["Stage"] === "Final")
+    .map(i => i["Away Team Goals"])
+);
+
 //(e) Winner of 2014 world cup final */
+
+let winner2014 = fifaData
+   .filter(i => i["Year"] === 2014 && i["Stage"] === "Final") 
+   .map(i => {
+     if(i["Home Team Goals"] > i["Away Team Goals"]){
+       return i["Home Team Name"];
+     } else { return i["Away Team Name"];      
+   }
+ });
+ console.log(winner2014);    
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -24,8 +54,8 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+
 }
 
 
@@ -47,7 +77,7 @@ Use the higher-order function getWinners to do the following:
 1. Receives an array
 2. Receives the callback function getFinals from task 2 
 3. Determines the winner (home or away) of each `finals` game. 
-4. Returns the names of all winning countries in an array called `winners` */ 
+4. Returns the names of all winning countries in an array called `winners` */
 
 function getWinners(/* code here */) {
     /* code here */
@@ -82,7 +112,7 @@ Use the higher order function getAverageGoals to do the following:
 */
 
 function getAverageGoals(/* code here */) {
-   /* code here */
+    /* code here */
 }
 
 
@@ -128,11 +158,11 @@ function badDefense(/* code here */) {
 
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
+function foo() {
     console.log('its working');
     return 'bar';
 }
-export default{
+export default {
     foo,
     getFinals,
     getYears,
